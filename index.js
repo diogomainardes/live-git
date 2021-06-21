06638fc0a9befc8304fe0f206257757516f3ac2f
Diogo Mainardes <diogomainardes@gmail.com>
2021-06-21 20:16:32 -0300

    Fiz este sozinho \o/

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const somar = (x, y) => {
     return x + y;
 }
 
+const exponencial = function (x, y) {
+    return x ** y
+}
+
 console.log('A soma é' + somar(10, 20));
+
+console.log(`O exponencial é ${exponencial(10, 3)}`);
